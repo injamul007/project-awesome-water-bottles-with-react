b4e7a8e744412031df663881aa089568cacc3abc
injamul007 <injamul007hoque@gmail.com>
2025-09-22 01:03:25 +0600

    add and load all the bottle by maping and add some css into the card bottle

---
+++ src/components/Bottle/Bottle.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Bottle.css"
+
+const Bottle = ({bottleData}) => {
+  // console.log(bottleData)
+  const {category,img,name,price} = bottleData
+  return (
+    <div className='bottle_card'>
+      <h2>Category: {category}</h2>
+      <img style={{width:'250px'}} src={img} alt='water_bottles_image' />
+      <h2>Name: {name}</h2>
+      <h2>Price: {price}</h2>
+    </div>
+  );
+};
+
+export default Bottle;
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { use } from 'react';
+import Bottle from '../Bottle/Bottle';
 
 const Bottles = ({bottlesPromise}) => {
   const bottlesData = use(bottlesPromise)
   return (
     <div>
       <h2>All the Bottles: {bottlesData.length}</h2>
+      {
+        bottlesData.map(bottleData => <Bottle 
+          key={bottleData.id} 
+          bottleData={bottleData}></Bottle>)
+      }
     </div>
   );
 };
