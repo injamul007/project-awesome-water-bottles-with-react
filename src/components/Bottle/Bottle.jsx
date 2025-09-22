f56f080531316c055a8e6d22c7499a6a5700f605
injamul007 <injamul007hoque@gmail.com>
2025-09-22 11:18:01 +0600

    add cart button and showing it to parent component that items is being added to cart

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./Bottle.css"
 
-const Bottle = ({bottleData}) => {
+const Bottle = ({bottleData,handleAddToCart}) => {
   // console.log(bottleData)
-  const {category,img,name,price} = bottleData
+  const {category,img,name,price,stock} = bottleData
   return (
     <div className='bottle_card'>
       <h2>Category: {category}</h2>
       <img src={img} alt='water_bottle_image' />
-      <h2>Name: {name}</h2>
-      <h2>Price: {price}</h2>
+      <h3>Name: {name}</h3>
+      <h3>Price: ${price}</h3>
+      <h3>In Stock: {stock}</h3>
+      <button onClick={()=>handleAddToCart(bottleData)}>Buy Now</button>
     </div>
   );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { use } from 'react';
 import Bottle from '../Bottle/Bottle';
 import "./Bottles.css"
 
 const Bottles = ({bottlesPromise}) => {
   const bottlesData = use(bottlesPromise)
+
+  const [cart, setCart] = useState([])
+
+  const handleAddToCart = (bottleData) => {
+    const newCart = [...cart, bottleData]
+    setCart(newCart)
+  }
   return (
     <div>
       <h2>All the Bottles: {bottlesData.length}</h2>
+      <p>Added to Cart: {cart.length}</p>
       <div className='bottles_container'>
         {
         bottlesData.map(bottleData => <Bottle 
           key={bottleData.id} 
-          bottleData={bottleData}></Bottle>)
+          bottleData={bottleData}
+          handleAddToCart={handleAddToCart}
+          ></Bottle>)
         }
       </div>
     </div>
